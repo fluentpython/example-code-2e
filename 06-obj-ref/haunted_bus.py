d63b4844f19eec4b376b32e2b5db2654c0d172ea
Luciano Ramalho <luciano@ramalho.org>
2020-02-19 00:12:02 -0300

    ch06: update from book draft

---
+++ 06-obj-ref/haunted_bus.py
@@ -0,0 +1,47 @@
+"""
+>>> bus1 = HauntedBus(['Alice', 'Bill'])
+>>> bus1.passengers
+['Alice', 'Bill']
+>>> bus1.pick('Charlie')
+>>> bus1.drop('Alice')
+>>> bus1.passengers
+['Bill', 'Charlie']
+>>> bus2 = HauntedBus()
+>>> bus2.pick('Carrie')
+>>> bus2.passengers
+['Carrie']
+>>> bus3 = HauntedBus()
+>>> bus3.passengers
+['Carrie']
+>>> bus3.pick('Dave')
+>>> bus2.passengers
+['Carrie', 'Dave']
+>>> bus2.passengers is bus3.passengers
+True
+>>> bus1.passengers
+['Bill', 'Charlie']
+
+
+>>> dir(HauntedBus.__init__)  # doctest: +ELLIPSIS
+['__annotations__', '__call__', ..., '__defaults__', ...]
+>>> HauntedBus.__init__.__defaults__
+(['Carrie', 'Dave'],)
+>>> HauntedBus.__init__.__defaults__[0] is bus2.passengers
+True
+
+"""
+
+# tag::HAUNTED_BUS_CLASS[]
+class HauntedBus:
+    """A bus model haunted by ghost passengers"""
+
+    def __init__(self, passengers=[]):  # <1>
+        self.passengers = passengers  # <2>
+
+    def pick(self, name):
+        self.passengers.append(name)  # <3>
+
+    def drop(self, name):
+        self.passengers.remove(name)
+# end::HAUNTED_BUS_CLASS[]
+
