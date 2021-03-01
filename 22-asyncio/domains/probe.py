5aaa4e49296a9e61b16074dcf57a290314eee224
Luciano Ramalho <luciano@ramalho.org>
2021-02-28 21:45:34 -0300

    ch22: examples

--- 22-asyncio/domains/probe.py
+++ 22-asyncio/domains/probe.py
@@ -8,8 +8,10 @@ from netaddr import multi_probe
 
 
 async def main(tld: str) -> None:
-    names = (f'{w}.{tld}'.lower() for w in kwlist if len(w) <= 4)
-    async for name, found in multi_probe(sorted(names)):
+    tld = tld.strip('.')
+    names = (w.lower() for w in kwlist if len(w) <= 4)
+    domains = (f'{name}.{tld}' for name in names)
+    async for name, found in multi_probe(domains):
         mark = '.' if found else '?\t\t'
         print(f'{mark} {name}')
 
