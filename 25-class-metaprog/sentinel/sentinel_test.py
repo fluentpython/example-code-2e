0ce109a9fef0e1a71576268b6469aa2b02fe3a9c
Luciano Ramalho <luciano@ramalho.org>
2021-05-24 13:27:07 -0300

    improved sentinel after learning from @taleinat on python-dev

--- 25-class-metaprog/sentinel/sentinel_test.py
+++ 25-class-metaprog/sentinel/sentinel_test.py
@@ -1,8 +1,12 @@
 import pickle
 
+import pytest
+
 from sentinel import Sentinel
 
-class PlainSentinel(Sentinel): pass
+
+class PlainSentinel(Sentinel):
+    pass
 
 
 class SentinelCustomRepr(Sentinel):
@@ -13,14 +17,21 @@ def test_repr():
     assert repr(PlainSentinel) == 'PlainSentinel'
 
 
-def test_pickle():
-    s = pickle.dumps(PlainSentinel)
-    ps = pickle.loads(s)
-    assert ps is PlainSentinel
+def test_cannot_instantiate():
+    with pytest.raises(TypeError) as e:
+        PlainSentinel()
+    msg = "'PlainSentinel' is a sentinel and cannot be instantiated"
+    assert msg in str(e.value)
 
 
 def test_custom_repr():
     assert repr(SentinelCustomRepr) == '***SentinelRepr***'
+
+
+def test_pickle():
+    s = pickle.dumps(SentinelCustomRepr)
+    ps = pickle.loads(s)
+    assert ps is SentinelCustomRepr
 
 
 def test_sentinel_comes_ready_to_use():
